f9936a026a566b29071556c7413eda20f0e5f569
Pfoerdie <pfoerdie@gmail.com>
2019-01-12 17:34:20 +0100

    wip

--- 0-1-8/PolicyAgent/PEP.js
+++ 0-1-8/PolicyAgent/PEP.js
@@ -12,6 +12,92 @@ const
     PDP = require('./PDP.js'),
     _enumerate = (obj, key, value) => Object.defineProperty(obj, key, { enumerable: true, value: value });
 
+/**
+ * @name _createRequestContext
+ * @param {Session} session
+ * @param {JSON} param
+ * @returns {RequestContext} requestContext
+ * @this {PEP}
+ * @private
+ */
+function _createRequestContext(session, param) {
+
+    // TODO alles durchgehen und überarbeiten!
+
+    const requestContext = {};
+
+    _enumerate(requestContext, '@type', "RequestContext");
+    _enumerate(requestContext, '@id', UUID());
+    _enumerate(requestContext, 'request', {});
+
+    /* 1. - add default subjects */
+
+    _enumerate(requestContext, 'target', param['target']);
+    if (param['assigner'] && typeof param['assigner']['@type'] === 'string')
+        _enumerate(requestContext, 'assigner', param['assigner']);
+    if (param['assignee'] && typeof param['assignee']['@type'] === 'string')
+        _enumerate(requestContext, 'assignee', param['assignee']);
+
+    /* 2. - add action requests */
+
+    const addRequest = (action) => {
+        const
+            requestID = `${action}-${UUID()}`,
+            actionDef = this.data.actionDefinition.get(action),
+            request = {};
+
+        _enumerate(request, 'id', requestID);
+        _enumerate(request, 'action', actionDef.action);
+
+        // if (actionDef.target && param[actionDef.target] && typeof param[actionDef.target]['@type'] === 'string')
+        //     _enumerate(request, 'target', param[actionDef.target]);
+        // if (actionDef.assigner && param[actionDef.assigner] && typeof param[actionDef.assigner]['@type'] === 'string')
+        //     _enumerate(request, 'assigner', param[actionDef.assigner]);
+        // if (actionDef.assignee && param[actionDef.assignee] && typeof param[actionDef.assignee]['@type'] === 'string')
+        //     _enumerate(request, 'assignee', param[actionDef.assignee]);
+
+        _enumerate(requestContext['request'], requestID, request);
+
+        if (actionDef.includedIn)
+            _enumerate(request, 'includedIn', addRequest(actionDef.includedIn));
+
+        _enumerate(request, 'implies', actionDef.implies.map(impl => addRequest(impl)));
+
+        return requestID;
+    }; // addRequest
+
+    let entryPoint = addRequest(param['action']);
+    _enumerate(requestContext, 'entryPoint', entryPoint);
+    return requestContext;
+
+} // _createRequestContext
+
+/**
+ * @name _validateAction
+ * @param {ResponseContext} responseContext
+ * @param {string} requestID
+ * @returns {boolean} validity
+ * @this {PEP}
+ * @private
+ */
+function _validateAction(responseContext, requestID) {
+    // TODO
+} // _validateAction
+
+/**
+ * @name _executeAction
+ * @param {ResponseContext} responseContext
+ * @param {string} requestID
+ * @param {...*} args
+ * @returns {*} result
+ * @this {PEP}
+ * @async
+ * @private
+ */
+async function _executeAction(responseContext, requestID, ...args) {
+    // TODO
+} // _executeAction
+
 async function _actionUse(target, impl, session) {
     return "USE";
 } // _actionUse
@@ -40,7 +126,7 @@ class PEP extends PolicyPoint {
         if (!(this.data.PDP instanceof PDP))
             this.throw(undefined, new Error(`PDP invalid`));
 
-        this.data.sessionMaxAge = 60 * 60 * 24 * 7;
+        this.data.sessionMaxAge = 60 * 60 * 24 * 14; // default: 14 days
         this.data.sessionStore = new SessionMemoryStore({
             expires: this.data.sessionMaxAge
         });
@@ -52,15 +138,23 @@ class PEP extends PolicyPoint {
         this.defineAction('transfer', _actionTransfer.bind(this));
     } // PEP.constructor
 
+    async generateSession() {
+
+    } // generateSession
+
     /**
      * @name PEP#request
-     * @param {Session} session
+     * @param {string} sessionID
      * @param {JSON} param
+     * @param {...*} args
      * @returns {*}
      * @async
      */
-    async request(session, param, args) {
-        if (!session || typeof session !== 'object' || typeof session.id !== 'string')
+    async request(sessionID, param, ...args) {
+        sessionID.save();
+        sessionID = sessionID.id; // TODO
+
+        if (!sessionID || typeof sessionID !== 'string')
             this.throw('request', new TypeError(`invalid argument`));
         if (!param || typeof param !== 'object')
             this.throw('request', new TypeError(`invalid argument`));
@@ -73,103 +167,55 @@ class PEP extends PolicyPoint {
         if (!this.data.actionDefinition.has(param['action']))
             this.throw('request', new Error(`action unknown`));
 
+        // gr0ßes TODO: wie mache ich das mit den Sessions?
+
+        // await new Promise((resolve, reject) => {
+        //     this.data.sessionStore.set(session.id, session, (err) => {
+        //         if (err) reject(err);
+        //         else resolve();
+        //     });
+        // });
+
+        const session = await new Promise((resolve, reject) => {
+            this.data.sessionStore.get(sessionID, (err, result) => {
+                if (err)
+                    return reject(err);
+
+                if (result)
+                    resolve(result);
+                else
+                    this.data.sessionStore.set(sessionID, {}, (err) => {
+                        if (err)
+                            return reject(err);
+
+                        this.data.sessionStore.get(sessionID, (err, result) => {
+                            if (err)
+                                return reject(err);
+
+                            if (result)
+                                resolve(result);
+                            else
+                                resolve();
+                        });
+                    });
+            });
+        });
+
         /* 1. - create RequestContext */
 
-        /** @type {RequestContext} */
-        const requestContext = {};
-
-        _enumerate(requestContext, '@type', "RequestContext");
-        _enumerate(requestContext, '@id', UUID());
-        _enumerate(requestContext, 'request', {});
-
-        /* 1.1. - add default subjects */
-
-        _enumerate(requestContext, 'target', param['target']);
-        if (param['assigner'] && typeof param['assigner']['@type'] === 'string')
-            _enumerate(requestContext, 'assigner', param['assigner']);
-        if (param['assignee'] && typeof param['assignee']['@type'] === 'string')
-            _enumerate(requestContext, 'assignee', param['assignee']);
-
-        /* 1.2. - add action requests */
-
-        const addRequest = (action) => {
-            const
-                requestID = `${action}-${UUID()}`,
-                actionDef = this.data.actionDefinition.get(action),
-                request = {};
-
-            _enumerate(request, 'id', requestID);
-            _enumerate(request, 'action', actionDef.action);
-
-            // if (actionDef.target && param[actionDef.target] && typeof param[actionDef.target]['@type'] === 'string')
-            //     _enumerate(request, 'target', param[actionDef.target]);
-            // if (actionDef.assigner && param[actionDef.assigner] && typeof param[actionDef.assigner]['@type'] === 'string')
-            //     _enumerate(request, 'assigner', param[actionDef.assigner]);
-            // if (actionDef.assignee && param[actionDef.assignee] && typeof param[actionDef.assignee]['@type'] === 'string')
-            //     _enumerate(request, 'assignee', param[actionDef.assignee]);
-
-            _enumerate(requestContext['request'], requestID, request);
-
-            if (actionDef.includedIn)
-                _enumerate(request, 'includedIn', addRequest(actionDef.includedIn));
-
-            _enumerate(request, 'implies', actionDef.implies.map(impl => addRequest(impl)));
-
-            return requestID;
-        }; // addRequest
-
-        const entryPoint = addRequest(param['action']);
+        let requestContext = _createRequestContext.call(this, session, param);
 
         /* 2. - send RequestContext to PDP#_decisionRequest */
 
         let responseContext = await this.data.PDP._decisionRequest(requestContext);
+        let entryPoint = requestContext['entryPoint'];
 
         /* 3. - execute Actions */
 
-        const executeAction = async (requestID, args) => {
-            let
-                tmpRequest = requestContext['request'][requestID],
-                requestStack = [tmpRequest];
+        if (!_validateAction.call(this, responseContext, entryPoint))
+            this.throw('request', new Error("request denied"));
 
-            while (tmpRequest.includedIn) {
-                tmpRequest = requestContext['request'][tmpRequest.includedIn];
-                requestStack.push(tmpRequest);
-            }
-
-            let
-                isTransfer = requestStack[requestStack.length - 1]['action'] === 'transfer',
-                tmpResult = isTransfer
-                    ? args
-                    : responseContext.subjects[responseContext['responses'][requestStack[0]['id']]['target']];
-
-            while (requestStack.length > 0) {
-                let
-                    currRequest = isTransfer ? requestStack.shift() : requestStack.pop(),
-                    currResponse = responseContext['responses'][currRequest['id']],
-                    callback = this.data.actionCallbacks.get(currResponse['action']),
-                    implObj = {};
-
-                for (let implReq of currRequest.implies) {
-                    _enumerate(
-                        implObj,
-                        responseContext['responses'][implReq]['action'],
-                        (args) => executeAction(implAction, args)
-                    );
-                } // for
-
-                tmpResult = await callback(tmpResult, implObj, session);
-            } // while
-
-            return tmpResult;
-
-            // TODO dieses Vorgehen ist nicht optimal
-
-        }; // executeAction
-
-        let result = await executeAction(entryPoint, args);
-
-        // TODO
-
+        let result = await _executeAction.call(this, responseContext, entryPoint, ...args);
         return result;
 
     } // PEP#request
@@ -184,6 +230,9 @@ class PEP extends PolicyPoint {
     defineAction(actionName, callback, includedIn, implies = []
         // , target = undefined, assigner = undefined, assignee = undefined
     ) {
+
+        // TODO überarbeiten!
+
         if (!actionName || typeof actionName !== 'string')
             this.throw('defineAction', new TypeError(`invalid argument`));
         if (typeof callback !== 'function')
