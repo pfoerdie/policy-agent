3896277261a025605fd7d1a1108f2d83bcc57e3e
Simon Petrac <pfoerdie@gmail.com>
2019-05-11 20:51:20 +0200

    wip

--- 0-1-8/Example/app.js
+++ 0-1-8/Example/app.js
@@ -1,6 +1,8 @@
 const
     Http = require('http'),
+    BodyParser = require('body-parser'),
     Express = require('express'),
+    ExpressSession = require('express-session'),
     Setup = require("./setup.js");
 
 Setup.ready(_main);
@@ -12,8 +14,40 @@ async function _main(policyAgent) {
         app = Express(),
         server = Http.createServer(app);
 
-    app.use(function (request, response, next) {
+    app.use(BodyParser.urlencoded({ extended: false }));
+    app.use(BodyParser.json());
 
+    app.use(ExpressSession({
+        secret: 'keyboard cat',
+        resave: false,
+        saveUninitialized: false,
+        cookie: { secure: false }
+    }));
+
+    app.get("/", function (request, response) {
+        let
+            param = {
+                action: "readFile",
+                target: {
+                    '@type': "File",
+                    '@id': "/"
+                }
+            },
+            session = request.session,
+            args = [response];
+
+        pep.request(param, session, ...args).then((result) => {
+            response.type('text/html').send(result);
+        }).catch(next);
+    });
+
+    app.use(function (request, response, next) {
+        let
+            param = request.body,
+            session = request.session,
+            args = [response];
+
+        pep.request(param, session, ...args).catch(next);
     });
 
     server.listen(80, () => console.log("running"));
