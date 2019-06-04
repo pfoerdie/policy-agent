58756b1836f7b12cd1ceb9b543f5eae1f521513c
Pfoerdie <pfoerdie@gmail.com>
2019-06-04 23:44:34 +0200

    PIP.registerType

--- 0-2-1/PolicyAgent/PIP.js
+++ 0-2-1/PolicyAgent/PIP.js
@@ -3,6 +3,9 @@ const
     _module = require("./index.js"),
     Context = require("./Context.js");
 
+let
+    _types = new Map();
+
 _.define(exports, '_cacheEntities', async function (context) {
     _.assert(context instanceof Context, "invalid context");
     _.assert(context.phase === 'cache_entities');
@@ -12,3 +15,10 @@ _.define(exports, '_cacheEntities', async function (context) {
 _.define(exports, '_getInformation', async function () {
     // TODO
 }); // PIP._getInformation
+
+_.define(exports, 'registerType', function (type, constructor) {
+    _.assert(type && typeof type === 'string');
+    _.assert(typeof constructor === 'function');
+    _.assert(!_types.has(type), "type already registered");
+    _types.set(type, callback);
+}); // PIP.registerType
